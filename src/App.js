940d1b01a543b4d1108ae48bc03e492f4481006a
rob99202 <rob99202@gmail.com>
2019-10-27 18:11:20 -1000

    Updated the function to a Component class that is returning code through a render function. Also updated the styles.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 import logo from "./blood-donation-svgrepo-com.svg";
 import {BrowserRouter as Router, Route}from "react-router-dom";
@@ -10,24 +10,34 @@ import DonorGet from "./components/donor-get.component";
 import ProductGet from "./components/product-get.component";
 import Home from "./components/account-registration.component";
 import Login from "./components/login.component";
+import DonorReport from "./components/donor-report.component"
 
-function App() {
-  return (
-     <Router>
-         <img src={logo} className="App-logo" alt="logo" width={80} height={80} mode='fit' vspace="20"/>
-         <div className="container">
-             <Navbar />
-             <br />
-                 <Route path="/Home" component={Home}/>
-                 <Route path="/edit-donor" component={DonorEditor}/>
-                 <Route path="/add-donor" component={DonorAdd}/>
-                 <Route path="/get-donor/:id" component={DonorGet}/>
-                 <Route path="/get-products" component={ProductGet}/>
-             <Route path="/login" component={Login}/>
-             <hr/>
-         </div>
-     </Router>
-  );
+class App extends Component {
+  render () {
+      return (
+      <Router>
+          <div className="App">
+              <div className="row">
+                  <div className="col-xs pl-3 pr-0  bg-dark mh-100">
+                      <img src={logo} className="App-logo" alt="logo" width={80} height={80} mode='fit' vspace="20"/>
+                  </div>
+                  <div className="col-xl-11">
+                      <Navbar/>
+                      <br/>
+                      <Route path="/Home" component={Home}/>
+                      <Route path="/edit-donor" component={DonorEditor}/>
+                      <Route path="/add-donor" component={DonorAdd}/>
+                      <Route path="/donor-report" component={DonorReport}/>
+                      <Route path="/get-donor/:id" component={DonorGet}/>
+                      <Route path="/get-products" component={ProductGet}/>
+                      <Route path="/login" component={Login}/>
+                      <hr/>
+                  </div>
+              </div>
+          </div>
+      </Router>
+      );
+  }
 }
 
 export default App;
